495de4df07d0787c55df004974cd03123fd11802
benji-unchangco <42650746+benji-unchangco@users.noreply.github.com>
2023-09-24 23:02:30 -0700

    Bottom navigation bar that switches between to activities

--- Doodl_test/app/src/main/java/com/example/doodl_test/FeedScreen.kt
+++ Doodl_test/app/src/main/java/com/example/doodl_test/FeedScreen.kt
@@ -1,16 +1,24 @@
 package com.example.doodl_test
 
+import android.graphics.drawable.Icon
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun FeedScreen() {
@@ -20,15 +28,24 @@ fun FeedScreen() {
             .background(Color.Magenta),
         contentAlignment = Alignment.Center
     ) {
-        Text(
+        /*Text(
             text = "Feed",
             fontSize = MaterialTheme.typography.h3.fontSize,
             fontWeight = FontWeight.Bold,
             color = Color.White
-        )
+        )*/
+
+        //Scrollable Lists
+        LazyColumn(modifier = Modifier.fillMaxSize()) {
+            items(20){i ->
+                androidx.compose.material.Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = null,
+                    modifier = Modifier.size(70.dp))
+            }
+        }
     }
 }
-
 @Composable
 @Preview
 fun FeedScreenPreview() {
