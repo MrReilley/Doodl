e04b5e062a05e772862bb73ab47206814ded7b35
benji-unchangco <42650746+benji-unchangco@users.noreply.github.com>
2023-09-24 23:03:49 -0700

    Added Scrollable List in Feed

--- Doodl_test/app/src/main/java/com/example/doodl_test/FeedScreen.kt
+++ Doodl_test/app/src/main/java/com/example/doodl_test/FeedScreen.kt
@@ -1,22 +1,22 @@
 package com.example.doodl_test
 
-import android.graphics.drawable.Icon
+//import android.graphics.drawable.Icon
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
+//import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+//import androidx.compose.material.Icon
+// androidx.compose.material.MaterialTheme
+//import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
+//import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
