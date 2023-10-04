cda3c18aafaa17a8c5708acf3ea4ad336569d2b3
Miklo13 <charles758@gmail.com>
2023-10-04 14:22:07 -0700

    Add Feed activity that will display all the images from firebase storage. Add logic to Respository and created a FeedViewModel for communication between repository and feed activity.

---
+++ app/src/main/java/com/example/doodl/FeedActivity.kt
@@ -0,0 +1,70 @@
+package com.example.doodl
+
+import android.graphics.Bitmap
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import com.example.doodl.viewmodel.FeedViewModel
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.graphics.painter.BitmapPainter
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModelProvider
+import com.example.doodl.data.Repository
+import com.example.doodl.ui.theme.DoodlTheme
+import com.example.doodl.viewmodel.FeedViewModelFactory
+import androidx.compose.runtime.livedata.observeAsState
+
+
+class FeedActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val repository = Repository()
+        val factory = FeedViewModelFactory(repository)
+        val feedViewModel = ViewModelProvider(this, factory)[FeedViewModel::class.java]
+
+        setContent {
+            DoodlTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    // Observe images LiveData and pass it to the ImageFeed composable.
+                    val images = feedViewModel.images.observeAsState(emptyList())
+                    ImageFeed(images.value)
+                }
+            }
+        }
+
+        // Fetch images once the activity is created.
+        feedViewModel.fetchImages()
+    }
+}
+
+@Composable
+fun ImageFeed(images: List<Bitmap>) {
+    // Display a vertical list of images.
+    LazyColumn {
+        items(images) { image ->
+            // For each image, create an Image composable
+            Image(
+                // Convert Bitmap to a format Image composable understands and renders it
+                painter = BitmapPainter(image.asImageBitmap()), // null implies decorative image (no alt text)
+                contentDescription = null,
+                // Style modifiers to control the layout and appearance of the image
+                modifier = Modifier.fillMaxWidth().aspectRatio(1f).padding(8.dp)
+            )
+        }
+    }
+}
--- app/src/main/java/com/example/doodl/data/Repository.kt
+++ app/src/main/java/com/example/doodl/data/Repository.kt
@@ -1,6 +1,10 @@
 package com.example.doodl.data
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import com.google.android.gms.tasks.Task
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.UploadTask
+
 
 // Code that interacts with Firebase Firestore and Firebase Firestorage
 // API for data access for rest of application
@@ -14,6 +18,40 @@ class Repository {
         val fileRef = storageReference.child("feed/${System.currentTimeMillis()}.png")
         // Upload byte array to Firebase Storage reference
         return fileRef.putBytes(byteArray)
+    }
+
+    fun downloadImage(imagePath: String): Task<Bitmap> {
+        // Get a reference to the image file at the specified path in Firebase Storage
+        val imageRef = storageReference.child(imagePath)
+        // Define a maximum size for the image to be downloaded (5MB here)
+        val maxSize: Long = 1024 * 1024 * 5
+        // Request the byte data of the image and, once it's available, process it
+        return imageRef.getBytes(maxSize).continueWith { task ->
+            // Check if task is unsuccessful and throw an exception if it is
+            if (!task.isSuccessful) {
+                throw task.exception ?: Exception("Unknown error")
+            }
+            // Convert the fetched byte data into a Bitmap
+            val bytes = task.result
+            val bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+            bitmap// Return the constructed Bitmap
+        }
+    }
+
+    fun fetchAllImages(onSuccess: (List<String>) -> Unit, onFailure: (Exception) -> Unit) {
+        // Reference to your 'feed' directory in Firebase storage
+        val feedRef = storageReference.child("feed")
+
+        // Retrieve all file references in 'feed' directory
+        feedRef.listAll().addOnSuccessListener { listResult ->
+            // Map the results to their paths and trigger the onSuccess callback
+            val imagePaths = listResult.items.map { it.path }
+            // Invoke onSuccess callback with the list of image paths
+            onSuccess(imagePaths.reversed())//.reversed will reverse
+        }.addOnFailureListener { exception ->
+            // Trigger onFailure callback on error
+            onFailure(exception)
+        }
     }
 
 }
