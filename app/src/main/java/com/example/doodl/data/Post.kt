4e46c2ed9195a1e632e5e3dbef30540399405d20
benji-unchangco <42650746+benji-unchangco@users.noreply.github.com>
2023-10-09 23:56:56 -0700

    Cleaned up User and Post.kt

--- app/src/main/java/com/example/doodl/data/Post.kt
+++ app/src/main/java/com/example/doodl/data/Post.kt
@@ -1,10 +1,2 @@
 package com.example.doodl.data
 
-data class Post(
-    val postId: String,
-    val userId: String,
-    val timestamp: Long,
-    val imagePath: String,
-    val likes: Int = 0,
-    val likedBy: List<String> = listOf()
-)
