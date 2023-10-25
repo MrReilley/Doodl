7d15d0da21d6601f996d0464cc7cbb5656d91831
benji-unchangco <42650746+benji-unchangco@users.noreply.github.com>
2023-10-24 20:12:29 -0700

    made a tab section that can swap between 2 firebase instances

--- app/src/main/java/com/example/doodl/ui/screens/ProfileActivity.kt
+++ app/src/main/java/com/example/doodl/ui/screens/ProfileActivity.kt
@@ -17,11 +17,18 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.itemsIndexed
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.TabRowDefaults
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
@@ -29,6 +36,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.graphics.painter.BitmapPainter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
@@ -39,7 +47,6 @@ import com.example.doodl.R
 import com.example.doodl.data.Repository
 import com.example.doodl.viewmodel.FeedViewModel
 import com.example.doodl.viewmodel.FeedViewModelFactory
-
 @Composable
 fun ProfileScreen() {
     BackHandler {
@@ -53,7 +60,6 @@ fun ProfileScreen() {
     }
     // Observe images LiveData and pass it to the ImageFeed composable.
     val images = feedViewModel.liveImages.observeAsState(emptyList())
-    //ProfilePosts(images.value)
     Column(
         modifier = Modifier.fillMaxSize()
     ){
@@ -65,8 +71,10 @@ fun ProfileScreen() {
                 // feed card layout
             Row(
                 verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceBetween
+                horizontalArrangement = Arrangement.Center
             ) {
+                Text(text = "")
+                Spacer(modifier = Modifier.width(130.dp))
                 Text(
                     text = "userName",
                     fontWeight = FontWeight.Bold,
@@ -75,9 +83,7 @@ fun ProfileScreen() {
                     textAlign = TextAlign.Center,
                     color = Color.Black
                 )
-                // logout
-                Spacer(modifier = Modifier.width(15.dp))
-
+                Spacer(modifier = Modifier.width(100.dp))
                 Text(
                     text = "logout",
                     fontWeight = FontWeight.Bold,
@@ -85,6 +91,7 @@ fun ProfileScreen() {
                     color = Color.Blue
                 )
             }
+
             Row(verticalAlignment = Alignment.CenterVertically) {
                 RoundImageCard(
                     image = R.drawable.likeicon,
@@ -92,10 +99,56 @@ fun ProfileScreen() {
                         .size(125.dp)
                         .padding(4.dp)
                 )
+                Text(
+                    text = "This is for the bio/description box for the template section of the profile page :)." +
+                            "I'm going to test the text wrapping now lol ewqdqdqdawdawdwadwadawddsawdawdasadawdasawdadawda",
+                    letterSpacing = 0.5.sp,
+                    lineHeight = 20.sp,
+                    softWrap = true,
+                    color = Color.Black
+                )
             }
-            //row bio
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                ProfilePosts(images = images.value)
+
+            var selectedTabIndex by remember { mutableStateOf(0) }
+            TabRow(
+                selectedTabIndex = selectedTabIndex,
+                indicator = { tabPositions ->
+                    TabRowDefaults.Indicator(
+                        modifier = Modifier.tabIndicatorOffset(tabPositions[selectedTabIndex]),
+                        color = Color.Blue // Customize the indicator color
+                    )
+                }
+            ) {
+                Tab(
+                    selected = selectedTabIndex == 0,
+                    onClick = { selectedTabIndex = 0 }
+                ){
+                    Image(
+                        painter =  painterResource(id = R.drawable.ic_grid),
+                        contentDescription = null
+                    )
+                }
+                Tab(
+                    selected = selectedTabIndex == 1,
+                    onClick = { selectedTabIndex = 1 }
+                ){
+                    Image(
+                        painter =  painterResource(id = R.drawable.likeicon),
+                        contentDescription = null
+                    )
+                }
+            }
+            when (selectedTabIndex) {
+                0 -> {
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        ProfilePosts(images = images.value)
+                    }
+                }
+                1 -> {
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        ProfilePosts(images = images.value)
+                    }
+                }
             }
         }
     }
@@ -103,7 +156,6 @@ fun ProfileScreen() {
 
 @Composable
 fun ProfilePosts(images: List<Bitmap>) {
-    //val context = LocalContext.current
     LazyVerticalGrid(
         columns = GridCells.Fixed(3)
     ) {
